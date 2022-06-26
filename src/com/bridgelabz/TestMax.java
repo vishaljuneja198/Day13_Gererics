b484b68a9af465a2b84d3d3ab55cfaa34de990e3
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-06-26 15:11:25 +0530

    master welcome message

---
+++ src/com/bridgelabz/TestMax.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class TestMax {
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Test of maximum 3 variables program ");
+
+    }
+}
+
