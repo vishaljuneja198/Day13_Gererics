e08de8c09e064635156a690472f24446e033f624
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-06-26 15:26:55 +0530

    added uc4 extended the max method more than three parameter

--- src/com/bridgelabz/TestMax.java
+++ src/com/bridgelabz/TestMax.java
@@ -1,6 +1,39 @@
 package com.bridgelabz;
 
 public class TestMax {
+    static String maxNumber(String a1, String a2, String a3, String a4) {
+        if (a1.compareTo(a2)>=1) {
+            if (a1.compareTo(a3)>=1) {
+                if (a1.compareTo(a4)>=1) {
+                    return a1;
+                } else {
+                    return a4;
+                }
+            } else if (a3.compareTo(a4)>=1) {
+                return a3;
+            } else {
+                return a4;
+            }
+        } else if (a2.compareTo(a3)>=1) {
+            if (a2.compareTo(a4)>=1) {
+                return a2;
+            } else {
+                return a4;
+            }
+        } else if (a3.compareTo(a4)>=1) {
+            return a3;
+        } else {
+            return a4;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Test of maximum 3 variables program ");
+
+        String a1 = "Hellojello", a2 = "hey", a4 = "welcome", a3 = "hello";
+
+        System.out.println("Biggest String is : " + maxNumber(a1, a2, a3, a4));
+    }
 
     static String maxNumber(String a1, String a2, String a3) {
         if (a1.compareTo(a2) >= 1) {
@@ -15,11 +48,4 @@ public class TestMax {
         }
     }
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Test of maximum 3 variables program ");
-
-        String a1 = "Hellojello", a2 = "hey", a3 = "welcome";
-
-        System.out.println("Biggest String is : " + maxNumber(a1, a2, a3));
-
-    }
+}
