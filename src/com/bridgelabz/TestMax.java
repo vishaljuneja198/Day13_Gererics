fb45cc6e078c5c4816822054e3f0a79a6ab9b69d
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-06-26 15:22:53 +0530

    added uc3 max string

--- src/com/bridgelabz/TestMax.java
+++ src/com/bridgelabz/TestMax.java
@@ -2,13 +2,13 @@ package com.bridgelabz;
 
 public class TestMax {
 
-    static float maxNumber(Float a1, Float a2, Float a3) {
-        if (a1.compareTo(a2) == 1) {
-            if (a1.compareTo(a3) == 1) {
+    static String maxNumber(String a1, String a2, String a3) {
+        if (a1.compareTo(a2) >= 1) {
+            if (a1.compareTo(a3) >= 1) {
                 return a1;
             } else
                 return a3;
-        } else if (a2.compareTo(a3) == 1) {
+        } else if (a2.compareTo(a3) >= 1) {
             return a2;
         } else {
             return a3;
@@ -18,9 +18,8 @@ public class TestMax {
     public static void main(String[] args) {
         System.out.println("Welcome to Test of maximum 3 variables program ");
 
-        Float a1 = 7.8f, a2 = 8.2f, a3 = 50.9f;
+        String a1 = "Hellojello", a2 = "hey", a3 = "welcome";
 
-        System.out.println("maximum number is : " + maxNumber(a1, a2, a3));
+        System.out.println("Biggest String is : " + maxNumber(a1, a2, a3));
 
     }
-}
