de1547ffa6f019e80981609b96a19d68131b7f68
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-06-26 15:30:19 +0530

    added uc5

--- src/com/bridgelabz/TestMax.java
+++ src/com/bridgelabz/TestMax.java
@@ -1,26 +1,27 @@
 package com.bridgelabz;
 
 public class TestMax {
-    static String maxNumber(String a1, String a2, String a3, String a4) {
-        if (a1.compareTo(a2)>=1) {
-            if (a1.compareTo(a3)>=1) {
-                if (a1.compareTo(a4)>=1) {
+    static <T extends Comparable<T>> T maxNumber(T a1, T a2, T a3, T a4) {
+
+        if (a1.compareTo(a2) >= 1) {
+            if (a1.compareTo(a3) >= 1) {
+                if (a1.compareTo(a4) >= 1) {
                     return a1;
                 } else {
                     return a4;
                 }
-            } else if (a3.compareTo(a4)>=1) {
+            } else if (a3.compareTo(a4) >= 1) {
                 return a3;
             } else {
                 return a4;
             }
-        } else if (a2.compareTo(a3)>=1) {
-            if (a2.compareTo(a4)>=1) {
+        } else if (a2.compareTo(a3) >= 1) {
+            if (a2.compareTo(a4) >= 1) {
                 return a2;
             } else {
                 return a4;
             }
-        } else if (a3.compareTo(a4)>=1) {
+        } else if (a3.compareTo(a4) >= 1) {
             return a3;
         } else {
             return a4;
@@ -34,18 +35,4 @@ public class TestMax {
 
         System.out.println("Biggest String is : " + maxNumber(a1, a2, a3, a4));
     }
-
-    static String maxNumber(String a1, String a2, String a3) {
-        if (a1.compareTo(a2) >= 1) {
-            if (a1.compareTo(a3) >= 1) {
-                return a1;
-            } else
-                return a3;
-        } else if (a2.compareTo(a3) >= 1) {
-            return a2;
-        } else {
-            return a3;
-        }
-    }
-
 }
