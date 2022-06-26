2321dcf084e61fbd7f455586fcec06cbc53af40b
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-06-26 15:17:41 +0530

    added uc2 greater from flaot numbers

--- src/com/bridgelabz/TestMax.java
+++ src/com/bridgelabz/TestMax.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public class TestMax {
 
-    static int maxNumber(Integer a1, Integer a2, Integer a3) {
+    static float maxNumber(Float a1, Float a2, Float a3) {
         if (a1.compareTo(a2) == 1) {
             if (a1.compareTo(a3) == 1) {
                 return a1;
@@ -18,8 +18,9 @@ public class TestMax {
     public static void main(String[] args) {
         System.out.println("Welcome to Test of maximum 3 variables program ");
 
-        Integer a1 = 7, a2 = 82, a3 = 50;
+        Float a1 = 7.8f, a2 = 8.2f, a3 = 50.9f;
 
         System.out.println("maximum number is : " + maxNumber(a1, a2, a3));
+
     }
 }
