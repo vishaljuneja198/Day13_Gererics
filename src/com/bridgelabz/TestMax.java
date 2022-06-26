7a821ead339ec5ed84c8f6ac6566abda71180f29
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-06-26 15:13:41 +0530

    added uc1 max at 3rd postion

--- src/com/bridgelabz/TestMax.java
+++ src/com/bridgelabz/TestMax.java
@@ -2,10 +2,24 @@ package com.bridgelabz;
 
 public class TestMax {
 
+    static int maxNumber(Integer a1, Integer a2, Integer a3) {
+        if (a1.compareTo(a2) == 1) {
+            if (a1.compareTo(a3) == 1) {
+                return a1;
+            } else
+                return a3;
+        } else if (a2.compareTo(a3) == 1) {
+            return a2;
+        } else {
+            return a3;
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Test of maximum 3 variables program ");
 
+        Integer a1 = 7, a2 = 82, a3 = 50;
+
+        System.out.println("maximum number is : " + maxNumber(a1, a2, a3));
     }
 }
-
